54ecb6f404b5e6b3ec9906c0216eaee71d88c994
xufucheng <xfcjscn@gmail.com>
2014-11-07 12:12:26 +0800

    signup

--- src/main/java/net/gplatform/spring/social/base/SimpleConnectionSignUp.java
+++ src/main/java/net/gplatform/spring/social/base/SimpleConnectionSignUp.java
@@ -24,25 +24,20 @@ package net.gplatform.spring.social.base;
 
 import java.util.UUID;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionSignUp;
-import org.springframework.social.connect.UserProfile;
 
 public class SimpleConnectionSignUp implements ConnectionSignUp {
 	final Logger logger = LoggerFactory.getLogger(SimpleConnectionSignUp.class);
 
+	/**
+	 * here use random generated id to act as localUserId, localUserId will be the key to save connection & sign
+	 */
 	@Override
 	public String execute(Connection<?> connection) {
-		UserProfile userProfile = connection.fetchUserProfile();
-		String localUserId = null;
-		if (StringUtils.isNotEmpty(userProfile.getUsername())) {
-			localUserId = userProfile.getUsername();
-		} else {
-			localUserId = UUID.randomUUID().toString();
-		}
+		String localUserId = UUID.randomUUID().toString();
 		logger.debug("Local User ID is: {}", localUserId);
 		return localUserId;
 	}
